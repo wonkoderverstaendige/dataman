01d6d7e1f67db15ccdc2be00ee87046415641dde
Ronny Eichler <R.Eichler@science.ru.nl>
2017-03-16 22:35:19 +0100

    Setup.py reformatting

--- setup.py
+++ setup.py
@@ -3,18 +3,12 @@ try:
 except ImportError:
     from distutils.core import setup
 
-config = {
-        'description': 'Data Manager',
-        'author': 'Ronny Eichler',
-        'url': '',
-        'download_url': '',
-        'author_email': 'ronny.eichler@gmail.com',
-        'version': '0.1.1',
-        'install_requires': ['nose', 'termcolor', 'vispy', 'numpy'],
-        'packages': ['dataman'],
-        'entry_points': """[console_scripts]
-            dm=dataman.dataman:main""",
-        'name': 'dataman'
-        }
-
-setup(**config)
+setup(name='dataman',
+      description='Data Manager',
+      author='Ronny Eichler',
+      author_email='ronny.eichler@gmail.com',
+      version='0.1.1',
+      install_requires=['nose', 'termcolor', 'vispy', 'numpy'],
+      packages=['dataman'],
+      entry_points="""[console_scripts]
+            dm=dataman.dataman:main""")
