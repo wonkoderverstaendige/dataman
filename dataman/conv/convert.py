978f94761a6b4b80db7449a3bc6cc8193b7a0f6d
Ronny Eichler <reichler@science.ru.nl>
2019-12-19 15:40:18 +0100

    Improve session file read in

--- dataman/conv/convert.py
+++ dataman/conv/convert.py
@@ -48,11 +48,13 @@ def expand_sessions(lot):
     for target_path in lot:
         if target_path.endswith('.session') or target_path.endswith('.txt'):
             with open(target_path, 'r') as sf:
-                session_items = [target_path.strip() for target_path in sf.readlines()]
+                session_items = [tp.strip() for tp in sf.readlines() if len(tp.strip())]
                 logger.info(f"Expanding session file '{target_path}' with {len(session_items)} targets")
                 targets.extend(session_items)
-        else:
+
+        elif len(target_path):
             targets.append(target_path)
+    logger.debug(f'Targets paths post expansion: {targets}')
     return targets
 
 
@@ -241,6 +243,19 @@ def main(args):
 
     # One of channel_count, channel_list, layout_file path from mutex parser group channel_group
     layout = None
+    layout_file = None
+
+    if cli_args.layout is not None:
+        layout_file = Path(cli_args.layout).resolve()
+
+    # if no .prb file specified, check if there is one matching the name of the session
+    else:
+        for target in cli_args.target:
+            layout_file = Path(target).with_suffix('.prb').resolve()
+            if layout_file.exists():
+                logger.warning(f'Using probe file {layout_file.name} matching session file name.')
+                break
+
     if cli_args.channel_count is not None:
         channel_groups = {0: {'channels': list(range(cli_args.channel_count)),
                               'dead_channels': dead_channels}}
@@ -249,9 +264,9 @@ def main(args):
         channel_groups = {0: {'channels': cli_args.channel_list,
                               'dead_channels': dead_channels}}
 
-    elif cli_args.layout is not None:
-        layout = util.run_prb(op.abspath(op.expanduser(cli_args.layout)))
-        logger.debug('Opened layout file {}'.format(layout))
+    elif layout_file:
+        logger.debug(f'Reading layout file {layout_file}')
+        layout = util.run_prb(layout_file)
         if cli_args.split_groups:
             channel_groups = layout['channel_groups']
             if 'dead_channels' in layout:
@@ -300,6 +315,8 @@ def main(args):
 
     out_fext = format_output.FMT_FEXT
     out_prefix = cli_args.out_prefix if cli_args.out_prefix is not None else op.basename(cli_args.target[0])
+    if '.' in out_prefix:
+        out_prefix = out_prefix.split('.')[0]
     logger.debug('Prefix: {}'.format(out_prefix))
     default_template = DEFAULT_FULL_TEMPLATE if cli_args.fname_channels else DEFAULT_SHORT_TEMPLATE
     fname_template = default_template if cli_args.template_fname is None else cli_args.template_fname
